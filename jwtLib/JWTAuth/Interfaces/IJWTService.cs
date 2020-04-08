0de571eaab3f1dcdfe8bd87cd060be28b35d6d2a
Stanislav Avdosev <Stanislav_Avdosev@epam.com>
2020-04-08 14:40:51 +0400

    refactoring

--- jwtLib/JWTAuth/Interfaces/IJWTService.cs
+++ jwtLib/JWTAuth/Interfaces/IJWTService.cs
@@ -8,7 +8,7 @@ namespace jwtLib.JWTAuth.Interfaces
     {
         Task<AllTokens> Refresh(string userId, string refreshToken);
         Task<AllTokens> Refresh(IJWTUser user);
-        Task<TokenData> GetCurrentIdFromToken(string authorizationToken);
+        Task<TokenData> GetCurrentDataFromToken(string authorizationToken);
         Task DeleteRefreshTokenFromUser(string userId, string refreshToken);
     }
 }
